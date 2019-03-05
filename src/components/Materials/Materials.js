877b9a1e8e411fc8b3ec0fb3c7d0c44e1bbb7178
SteveFWagner <sfwagner1011@gmail.com>
2019-03-05 09:51:31 -0700

    initial

---
+++ src/components/Materials/Materials.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class Materials extends Component{
+    render(){
+        return(
+            <div>
+                Materials
+            </div>
+        )
+    }
+
+}
+export default Materials
